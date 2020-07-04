5c0b50252affbc5fd86f59d1fc08a02b7393d734
Jacob Smith <js@jacobsmith.tech>
2020-07-04 13:15:22 -0700

    Reindent to match eslint

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
 /* eslint-disable no-mixed-operators */
 
 module.exports = (vertices) => {
-    let centroidX = 0.0
-    let centroidY = 0.0
-    let signedArea = 0.0
-    let x0 = 0.0
-    let y0 = 0.0
-    let x1 = 0.0
-    let y1 = 0.0
-    let a = 0.0
-    let vertex0
-    let vertex1
+  let centroidX = 0
+  let centroidY = 0
+  let signedArea = 0
+  let x0 = 0
+  let y0 = 0
+  let x1 = 0
+  let y1 = 0
+  let a = 0
+  let vertex0
+  let vertex1
 
-    let i
-    let vertexCount = vertices.length
-    for (i = 0; i < vertexCount - 1; i++) {
-        vertex0 = vertices[i]
-        vertex1 = vertices[i + 1]
+  let i
+  let vertexCount = vertices.length
+  for (i = 0; i < vertexCount - 1; i++) {
+    vertex0 = vertices[i]
+    vertex1 = vertices[i + 1]
 
-        x0 = vertex0[0]
-        y0 = vertex0[1]
-        x1 = vertex1[0]
-        y1 = vertex1[1]
+    x0 = vertex0[0]
+    y0 = vertex0[1]
+    x1 = vertex1[0]
+    y1 = vertex1[1]
 
-        a = x0 * y1 - x1 * y0
-        signedArea += a
-        centroidX += (x0 + x1) * a
-        centroidY += (y0 + y1) * a
-    }
-
-    // Do last vertex separately for efficiency's sake
-    x0 = vertices[vertices.length - 1][0]
-    y0 = vertices[vertices.length - 1][1]
-    x1 = vertices[0][0]
-    y1 = vertices[0][1]
     a = x0 * y1 - x1 * y0
     signedArea += a
     centroidX += (x0 + x1) * a
     centroidY += (y0 + y1) * a
+  }
 
-    signedArea *= 0.5
-    let a6 = 6.0 * signedArea
-    centroidX /= a6
-    centroidY /= a6
+  // Do last vertex separately for efficiency's sake
+  x0 = vertices[vertices.length - 1][0]
+  y0 = vertices[vertices.length - 1][1]
+  x1 = vertices[0][0]
+  y1 = vertices[0][1]
+  a = x0 * y1 - x1 * y0
+  signedArea += a
+  centroidX += (x0 + x1) * a
+  centroidY += (y0 + y1) * a
 
-    return [centroidX, centroidY]
+  signedArea *= 0.5
+  let a6 = 6 * signedArea
+  centroidX /= a6
+  centroidY /= a6
+
+  return [centroidX, centroidY]
 }
--- test.js
+++ test.js
@@ -4,39 +4,39 @@ const findCentroid = require('./index.js')
 const expect = require('chai').expect
 
 let polygon = [
-    [0, 0],
-    [5, 0],
-    [5, 5],
-    [0, 5],
+  [0, 0],
+  [5, 0],
+  [5, 5],
+  [0, 5],
 ]
 
 let badPolygon = [
-    ['a', {}],
-    [1, []],
-    [null, 0],
+  ['a', {}],
+  [1, []],
+  [null, 0],
 ]
 
 describe('Centroid 2D', function() {
 
-    describe('On the subject of finding a centroid, it ', function() {
+  describe('On the subject of finding a centroid, it ', function() {
 
-        it('should result in an Array.', function() {
-            let centroid = findCentroid(polygon)
-            expect(centroid).to.be.an.instanceof(Array)
-        })
-
-        it('should result in the proper coordinates', function() {
-            let centroid = findCentroid(polygon)
-            expect(centroid[0]).to.equal(2.5)
-            expect(centroid[1]).to.equal(2.5)
-        })
-
-        it('should not throw when passed bad arguments', function() {
-            expect(() => {
-                findCentroid(badPolygon)
-            }).not.to.throw(Error)
-        })
-
+    it('should result in an Array.', function() {
+      let centroid = findCentroid(polygon)
+      expect(centroid).to.be.an.instanceof(Array)
     })
+
+    it('should result in the proper coordinates', function() {
+      let centroid = findCentroid(polygon)
+      expect(centroid[0]).to.equal(2.5)
+      expect(centroid[1]).to.equal(2.5)
+    })
+
+    it('should not throw when passed bad arguments', function() {
+      expect(() => {
+        findCentroid(badPolygon)
+      }).not.to.throw(Error)
+    })
+
+  })
 
 })
